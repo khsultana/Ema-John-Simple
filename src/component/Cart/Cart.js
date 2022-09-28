9aaf435d4dd59be0c6eef59fb599d51b8613f090
Khan Sultana <Khan.Sultana@fifo-tech.com>
2022-09-28 18:29:16 +0600

    first commit

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+    console.log(cart)
     return (
-        <div>
+        <div className='cart'>
 
 
             <h4>Order Summary </h4>
-            <p>Selected : {cart.length}</p>
+            <p>Selected Items : {cart.length}</p>
+            <p>Total Price: </p>
+            <p>Total Shipping: </p>
+            <p>Tax: </p>
+            <h4>Grand Total: </h4>
 
         </div>
     );
